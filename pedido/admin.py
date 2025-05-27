3140632c9cf188109f5c87325072d7de5e370eaf
Graciliana Kascher <eagraciliana@gmail.com>
2025-05-27 13:21:38 -0300

    [feat] create models pedido and itempedido

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import ItemPedido, Pedido
+from . import models
 
-# Register your models here.
+
+class ItemPedidoInline(admin.TabularInline):
+    model = models.ItemPedido
+    extra = 1
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines = [ItemPedidoInline]
+
+
+admin.site.register(Pedido, ProdutoAdmin)
+admin.site.register(ItemPedido)
